3efbb7d5812328d030ea9501c970d540ef5461a5
Conrad Etherington <conrad.etherington@gmail.com>
2023-02-07 23:32:08 -0700

    almost? need to carry the 1

--- multiply_strings.js
+++ multiply_strings.js
@@ -21,67 +21,44 @@ function testFunction(nums1, nums2) {
   }
  assignPlaces(nums1, nums2)
  console.log(`PLaces Assigned topArr=${topArr} OOOOO botArr=${botArr}`)
+ console.log(topArr)
+ console.log(botArr)
 // let topArr = ["5","1","2","3"]
 // let botArr = ["1","2","3"]
 
 
-let topLength = topArr.length
-let botLength = botArr.length
-let topMultiplier = 1
-let botMultiplier = 1
-
-// train computer to assembnle bottom number as shortest number of digits
-
-let topNumber = []
-let botNumber = []
-for (let topIdx = topArr.length -1 ; topIdx >= 0; topIdx--) {
-   topNumber.unshift(`${topArr[topIdx]*topMultiplier}`);
-  topMultiplier *= 10
-}
-
-for (let botIdx = botArr.length -1 ; botIdx >= 0; botIdx--) {
-  botNumber.unshift(`${botArr[botIdx]*botMultiplier}`);
- botMultiplier *= 10
-}
-
-console.log(topNumber)
- console.log(botNumber)
-// for (let )
-// for loop through top number by the bottom number
-// let counter = 1
-// for (let botIdx = botArr.length -1; botIdx >= 0; botIdx --)
-// {
-//   //
-//   total += botArr[botIdx]*botMultiplier*topArr[topArr.length - counter]
-//   console.log(`step ${botArr[botIdx]*botMultiplier*topArr[topArr.length - counter]}`)
-//   console.log(total)
-//   botMultiplier *= 10
-// }
-// iterate over bottom array to multiply each element of botNum by every element of topNum
-let subTot = 0
-let total = 0
-// for (let botNum of botNumber) {
-//   botNum * 
-// }
-
- botNumber.reverse().forEach((num)=> {
-  console.log(num)
-  //num * each value in topNum
-  topNumber.forEach((topNum) => {
-    total += topNum * num
+// train computer to assemble bottom number as shortest number of digits
+botIdx = botArr.length -1
+topIdx = topArr.length -1
+let zerocunt = 0
+console.log("X_____________________________________")
+botArr.forEach((botDigit) => {
+  let currentLineArr = []
+  topArr.reverse().forEach((digit)=>{
+    currentLineArr.unshift(digit*botArr[botIdx])
+    
   })
-  console.log(total)
- })
+  // add zerocount number of zeros to the array.
+let zerocounter = zerocunt
+while(zerocounter > 0) {
+  currentLineArr.push(0)
+  zerocounter -= 1
+}
+  console.log(currentLineArr)
+  // console.log(zerocunt)
+  zerocunt +=1
+})
 
-//   topNumber.reverse().forEach((topNum) => {
-//     subTot += num * topNum 
-//     //console.log(num)
-//   })
-//total += subTot
-// console.log(subTot)
-// })
-// console.log("BILLY")
-// console.log(subTot)
+// let firstLine = botArr[botIdx]* topArr[topIdx]
+
+// firstLineArr.unshift(firstLine)
+//console.log(firstLineArr)
+
+
 }
 
-testFunction(["1","2","3","4","5","6","7","8","9"], ["9","8","7","6","5","4","3","2","1"])
+testFunction(["1","2","3","4","5"], ["1","2","3"])
+
+
+// i'm thinking we structure the whole thing in arrays
+//We're gonna run through it more humanlike
